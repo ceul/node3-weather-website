fe3a280204df238ee5a4e3cea3b0b163d4088ac6
ceul <ceul@ceul.ceul.com>
2019-03-15 16:12:41 -0500

    Change message in forecast to spanish

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. there is a ${body.currently.precipProbability}% chance of rain`)
+            callback(undefined,`${body.daily.data[0].summary} Actualmente la temperatura es de ${body.currently.temperature} grados. Existe una posibilidad del  ${body.currently.precipProbability}% de lluvia`)
         }    
     })
 }
